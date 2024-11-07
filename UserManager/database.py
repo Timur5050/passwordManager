33a30f2a229479759c57344907503918b4547eae
Timur <stukantimur811@gmail.com>
2024-11-07 23:00:44 +0200

    created async engine and session

--- UserManager/database.py
+++ UserManager/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 
 from settings import settings
@@ -8,16 +8,16 @@ from settings import settings
 engine = create_engine(
     settings.DATABASE_URL,
 )
-async_engine = create_engine(
-    settings.ASYNC_DATABASE_URL
+async_engine = create_async_engine(
+    settings.DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://"),
+    echo=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-AsyncSession = sessionmaker(
+AsyncSessionLocal = sessionmaker(
     bind=async_engine,
     class_=AsyncSession,
     autocommit=False,
-    autoflush=False,
+    autoflush=False
 )
-
 Base = declarative_base()
