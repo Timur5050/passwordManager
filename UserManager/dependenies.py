ffa8dfdb9f78cdf0919431c27ff8d786e7741d75
Timur <stukantimur811@gmail.com>
2024-11-07 23:01:24 +0200

    crated async dependency to connect to db

--- UserManager/dependenies.py
+++ UserManager/dependenies.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from database import SessionLocal
+from database import SessionLocal, AsyncSessionLocal
 
 
 def get_db() -> Session:
@@ -10,3 +11,8 @@ def get_db() -> Session:
         yield db
     finally:
         db.close()
+
+
+async def get_async_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
