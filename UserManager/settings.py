09dd270d5180d2d5517275f05536e66c2db60e23
Timur <stukantimur811@gmail.com>
2024-11-07 23:01:50 +0200

    created class to manage redis connection

--- UserManager/settings.py
+++ UserManager/settings.py
@@ -1,4 +1,6 @@
 import os
+
+import redis
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -10,11 +12,23 @@ load_dotenv(dotenv_path=env_path)
 
 
 class AuthJWT(BaseSettings):
-    private_key_path: Path = BASE_DIR / "UserManager" / "certs" / "private.pem"
-    public_key_path: Path = BASE_DIR / "UserManager" / "certs" / "public.pem"
+    # private_key_path: Path = BASE_DIR / "shop_manager" / "certs" / "private.pem"
+    private_key_path = Path(os.path.expanduser("~/Documents/important/projects/shops_manager/certs/private.pem"))
+    public_key_path = Path(os.path.expanduser("~/Documents/important/projects/shops_manager/certs/public.pem"))
+    # public_key_path: Path = BASE_DIR / "shop_manager" / "certs" / "public.pem"
     algorithm: str = "RS256"
     access_token_exp_minutes: int = 15
     refresh_token_exp_minutes: int = 60 * 24
+
+
+class RedisCacheDataBase:
+    def __init__(self, host='localhost', port=6379, db=0):
+        self.host = host
+        self.port = port
+        self.db = db
+
+    def get_redis_client(self) -> redis.Redis:
+        return redis.Redis(host=self.host, port=self.port, db=self.db)
 
 
 class Settings(BaseSettings):
@@ -28,8 +42,8 @@ class Settings(BaseSettings):
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     # DATABASE_URL: str | None = "sqlite:///./user_manager.sqlite3"
-    ASYNC_DATABASE_URL: str | None = "sqlite+aiosqlite:///./user_manager.sqlite3"
     auth_jwt: AuthJWT = AuthJWT()
+    redis: RedisCacheDataBase = RedisCacheDataBase()
 
     class Config:
         case_sensitive = True
