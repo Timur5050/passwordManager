e55a93c4ed28c8ef120c6361936b3036535c2e70
Timur <stukantimur811@gmail.com>
2024-11-11 11:30:23 +0200

    deleted redundant method

--- UserManager/main.py
+++ UserManager/main.py
@@ -8,17 +8,3 @@ from auth.utils import decode_jwt
 app = FastAPI()
 
 app.include_router(user_router.router)
-
-
-@app.get("/")
-async def root(request: Request):
-    headers = request.headers
-    token = headers.get("Authorization").replace("Bearer ", "")
-    try:
-        content = decode_jwt(token)
-    except jwt.exceptions.ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Expired token")
-
-    response = JSONResponse(content=content)
-    response.set_cookie(key="hello", value="how are you")
-    return response
