db86cb23ecc1b46ad04563f2db639171e24483c7
Timur <stukantimur811@gmail.com>
2024-11-15 00:05:41 +0200

    separated into two functions from one

--- UserManager/auth/middleware.py
+++ UserManager/auth/middleware.py
@@ -4,12 +4,7 @@ from fastapi import Request, HTTPException
 from auth.utils import decode_jwt
 
 
-def get_user_id(request: Request):
-    headers = request.headers
-    try:
-        token = headers.get("Authorization").replace("Bearer ", "")
-    except (KeyError, AttributeError):
-        raise HTTPException(status_code=400, detail="Invalid Authorization header")
+def get_user_id_by_the_token(token: str) -> int:
     try:
         content = decode_jwt(token)
     except jwt.exceptions.ExpiredSignatureError:
@@ -18,3 +13,13 @@ def get_user_id(request: Request):
         raise HTTPException(status_code=401, detail="Invalid token")
 
     return content["user_id"]
+
+
+def get_user_id(request: Request):
+    headers = request.headers
+    try:
+        token = headers.get("Authorization").replace("Bearer ", "")
+    except (KeyError, AttributeError):
+        raise HTTPException(status_code=400, detail="Invalid Authorization header")
+
+    return get_user_id_by_the_token(token)
