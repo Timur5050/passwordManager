ce025806d54050720b6c11461de1f9376bb183b6
Timur <stukantimur811@gmail.com>
2024-11-18 12:28:29 +0200

    Created dependecies file for password manager service

---
+++ PasswordManager/dependencies.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import SessionLocal, AsyncSessionLocal
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+async def get_async_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
